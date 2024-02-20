6d638c1932f2eeca1b78366625d36783089de8c5
Zainab Mohamed <142171425+zeemohamed7@users.noreply.github.com>
2024-02-21 00:06:21 +0300

    add signup/login page UI

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,7 +1,92 @@
-import React from 'react'
-
+import React from "react";
+import vectorLogo from "../images/vector-logo.png";
+// Log in and sign up here
 export default function HomePage() {
+
+  
   return (
-    <div>HomePage</div>
-  )
+      <div class="container-xl">
+        <div class="homepage-form">
+          <h2 class="signup-title">Sign Up for vector</h2>
+          <form> 
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">
+                Name
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" class="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div class="mb-3 form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1">
+              I agree to the <u>terms & policy</u>
+              </label>
+            </div>
+  <button class="btn blob-btn">
+    Sign Up
+    <span class="blob-btn__inner">
+      <span class="blob-btn__blobs">
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+      </span>
+    </span>
+  </button>
+  <br/>
+
+<svg class='svg-btn' xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <defs>
+    <filter id="goo">
+      <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+      <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+    </filter>
+  </defs>
+</svg>
+<button class="google-signin-button">
+  <img src='https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png' class="icon" />
+  Sign in with Google
+</button>
+          </form>
+         <p>Have an account? <a href class="signin-redirect">Sign In</a></p>
+
+        </div>
+        <div class="vector-container">
+          <img class="vector-logo" src={vectorLogo} />
+        </div>
+      </div>
+  );
 }

612e532e861b0a6d34a07df5716c06904178bab5
Zainab Mohamed <142171425+zeemohamed7@users.noreply.github.com>
2024-02-28 21:04:24 +0300

    create group chat

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -62,6 +62,8 @@ const accessChat = asyncHandler(async (req, res) => {
         }
 })
 
+
+
 const fetchChats = asyncHandler(async (req, res) => {
     try {
         // Look through all chats that contains user that is logged in
@@ -73,7 +75,7 @@ const fetchChats = asyncHandler(async (req, res) => {
         .then(async (results) => {
           results = await User.populate(results, {
             path: "latestMessage.sender",
-            select: "name pic email",
+            select: "name picture email",
           });
           res.status(200).send(results);
         });
@@ -83,4 +85,41 @@ const fetchChats = asyncHandler(async (req, res) => {
     }
   });
 
-module.exports = {accessChat, fetchChats}
+
+
+
+const createGroup = asyncHandler(async (req, res) => {
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please fill all the fields!" });
+      }
+    
+      // Send users for users to choose to add
+      let users = JSON.parse(req.body.users);
+    
+      if (users.length < 2) {
+        return res
+          .status(400)
+          .send("Select more users to create a group chat!");
+      }
+    
+      users.push(req.user);  // Add logged in user too
+    
+      try {
+        const groupChat = await Chat.create({
+          chatName: req.body.name,
+          users: users,
+          isGroupChat: true,
+          groupAdmin: req.user,
+        });
+    
+        const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+          .populate("users", "-password")
+          .populate("groupAdmin", "-password");
+    
+        res.status(200).json(fullGroupChat);
+      } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+      }
+})
+module.exports = {accessChat, fetchChats, createGroup}
--- backend/routes/chatsRoutes.js
+++ backend/routes/chatsRoutes.js
@@ -7,7 +7,7 @@ const { protect } = require("../middleware/authMiddleware");
 
 router.post('/chat', protect, chatsControllers.accessChat)
 router.get('/chats', protect, chatsControllers.fetchChats)
-// router.post('/group', protect, chatsControllers.createGroup)
+router.post('/group', protect, chatsControllers.createGroup)
 // router.put('/group-rename', protect, chatsControllers.renameGroup)
 // router.put('/group-add', protect, chatsControllers.addToGroup)
 // router.put('/group-remove', protect, chatsControllers.removeFromGroup)

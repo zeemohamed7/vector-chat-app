c5c919145d37b4c37d3974774076a85cbba2dad6
Zainab Mohamed <142171425+zeemohamed7@users.noreply.github.com>
2024-02-28 21:07:41 +0300

    add other group functionalities: rename, remove, add

--- backend/routes/chatsRoutes.js
+++ backend/routes/chatsRoutes.js
@@ -8,8 +8,8 @@ const { protect } = require("../middleware/authMiddleware");
 router.post('/chat', protect, chatsControllers.accessChat)
 router.get('/chats', protect, chatsControllers.fetchChats)
 router.post('/group', protect, chatsControllers.createGroup)
-// router.put('/group-rename', protect, chatsControllers.renameGroup)
-// router.put('/group-add', protect, chatsControllers.addToGroup)
-// router.put('/group-remove', protect, chatsControllers.removeFromGroup)
+router.put('/group-rename', protect, chatsControllers.renameGroup)
+router.put('/group-add', protect, chatsControllers.addToGroup)
+router.put('/group-remove', protect, chatsControllers.removeFromGroup)
 
 module.exports = router;
